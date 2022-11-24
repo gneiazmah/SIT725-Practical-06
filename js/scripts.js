ed560ce86bbfd1e1dc29225d031d929ad416645f
G.azzu <gneiazmah@gmail.com>
2022-11-24 23:51:12 +1100

    Change-2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 function changeText(){
-    var textsArray = ["Test 1", "Test 2", "Test 3", "Test 4", "Test 5"]
+    var textsArray = ["Select 1", "Select 2", "Select 3", "Select 4", "Select 5"]
     var number = getRandomNumberBetween(0,textsArray.length-1)
     console.log("Index: ", number)
     document.getElementById("heading").innerHTML= textsArray[number];
