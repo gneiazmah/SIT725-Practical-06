dd550ff80bb37196ed8caa6f5950d32cb1e1799b
G.azzu <gneiazmah@gmail.com>
2022-12-01 00:22:14 +1100

    change-2

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function changeText(){
-    var textsArray = ["Select 1", "Select 2", "Select 3", "Select 4", "Select 5"]
-    var number = getRandomNumberBetween(0,textsArray.length-1)
-    console.log("Index: ", number)
-    document.getElementById("heading").innerHTML= textsArray[number];
-}
-function getRandomNumberBetween(min,max){
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
