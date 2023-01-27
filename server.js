de5cce8f07ff2f7a292a50dd424d16edede853c9
G.azzu <gneiazmah@gmail.com>
2023-01-27 16:40:48 +1100

    change 13

--- server.js
+++ server.js
@@ -16,6 +16,25 @@ app.use(cors())
 
 app.use('/api/projects',projectRoutes)
 
+
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+
+    var firstNumber = parseInt(req.params.firstNumber)
+  
+    var secondNumber = parseInt(req.params.secondNumber)
+  
+    var result = firstNumber + secondNumber || null
+  
+    if(result == null) {
+  
+      res.json({result: result, statusCode: 400}).status(400)
+  
+    }
+    
+    else { res.json({result: result, statusCode: 200}).status(200) }
+  
+  })
+
 //mongoDB action
 
 //const MongoClient = require('mongodb').MongoClient;
@@ -104,7 +123,7 @@ const cardList = [
 //     })
 // })
 
-var port = process.env.port || 3000;
+var port = process.env.port || 8080;
 
 app.listen(port,()=>{
     console.log("App listening to: http://localhost:"+port)
--- test/test.js
+++ test/test.js
@@ -1,5 +1,18 @@
 let express = require(express);
+let app = express();
 var expect = require("chai").expect;
+const { request } = require("express");
 var requst = require("request");
 
+describe("Add two numbers", function(){
+    var url = 'http://localhost:8080/addTwoNumbers/3/5';
 
+    it("returns status 200 to check if api works", function(done){
+
+        request(url, function(error, response){
+            expect(response.statusCode).to.equal(200);
+            document();
+        });
+    });
+
+});
